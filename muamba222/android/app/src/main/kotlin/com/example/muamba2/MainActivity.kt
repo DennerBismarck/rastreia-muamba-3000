b84f420272a68da8cf12044306d06ba138104abe
guilherme <guimedeiroxdev@gmail.com>
2024-08-12 23:06:41 -0300

    Remove duplicated directory

--- muamba222/android/app/src/main/kotlin/com/example/muamba2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.muamba2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- muamba222/lib/controllers/theme_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class ThemeController extends GetxController {
-  ThemeData get themeData =>
-      Get.isDarkMode ? ThemeData.dark() : ThemeData.light();
-}
